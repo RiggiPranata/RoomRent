da0a4a8602bb1bb323d3bfb6c57d89a7da429b23
warno  purnomo <warno.purnomo@diamond.co.id>
2022-12-25 04:01:33 +0700

    create screen login and regist

--- app/src/main/java/com/example/roomrent/MainActivity.java
+++ app/src/main/java/com/example/roomrent/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.roomrent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
 
 import android.os.Bundle;
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void signUp(View v) {
+        Intent intent = new Intent(MainActivity.this, registratiton.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/roomrent/registratiton.java
@@ -0,0 +1,14 @@
+package com.example.roomrent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registratiton extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regitratiton);
+    }
+}
